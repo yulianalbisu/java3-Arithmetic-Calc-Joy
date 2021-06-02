f24a66ec41433c0f87089369f14f183deb986070
Yuliana Albisu <ya.aldrich@yahoo.com>
2021-06-02 15:48:16 +0000

    arithmetic done

---
+++ calc/Arithmetic.java
@@ -0,0 +1,38 @@
+package calc;
+import java.lang.Math;
+import java.util.Scanner;
+
+
+public class Arithmetic {
+	public static float add(float num1, float num2) {
+		return num1+num2;
+	}
+
+	public static float subtract(float num1, float num2) {
+		return num1-num2;
+	}
+
+	public static float multiply(float num1, float num2) {
+		return num1*num2;
+	}
+
+	public static float divide(float num1, float num2) {
+		return num1/num2;
+	}
+
+	public static float square(float num1) {
+		return num1*num1;
+	}
+
+	public static float cube(float num1) {
+		return num1*num1*num1;
+	}
+
+	public static float power(float num1, float num2) {
+		return (int) Math.pow(num1, num2);
+	}
+
+	public static float mod(float num1, float num2) {
+		return num1%num2;
+	}
+}
